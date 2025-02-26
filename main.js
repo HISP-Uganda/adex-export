99cca31ef03ff3331c62e37882746e4454079130
Charles Olupot <olupotcharles@gmail.com>
2025-02-26 14:50:38 +0300

    CHange approach

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const dotenv = require("dotenv");
-const { chunk, orderBy } = require("lodash");
+const { chunk, orderBy, uniqBy, uniq } = require("lodash");
 const Papa = require("papaparse");
 
 dotenv.config();
@@ -70,6 +70,7 @@ class DHIS2DataTransfer {
             fields,
             paging: false,
             level,
+            filter: `name:in:[Adjumani District,Alebtong District,Amolatar District,Amudat District,Amuria District,Amuru District,Apac District,Bukwo District,Bulambuli District,Busia District,Butambala District,Butebo District,Kyenjojo District,Kyotera District,Lamwo District,Mpigi District]`,
         };
 
         try {
@@ -81,23 +82,51 @@ class DHIS2DataTransfer {
             );
         }
     }
-    async fetchDataElements() {
+    async fetchDataElements(dataSets) {
         console.log("Fetching data elements...");
         const url = `/api/dataSets.json`;
         const params = {
-            fields: "id,dataSetElements[dataElement[id,name]]",
+            fields: "id,dataSetElements[dataElement[id,name]],organisationUnits[id,name]",
+            paging: false,
+            filter: `id:in:[${dataSets.join(",")}]`,
+        };
+        let organisationUnits = [];
+        let dataElements = [];
+        try {
+            const { data } = await this.destApi.get(url, { params });
+            data.dataSets.forEach((ds) => {
+                dataElements = dataElements.concat(
+                    ds.dataSetElements.map((de) => de.dataElement.id),
+                );
+                organisationUnits = organisationUnits.concat(
+                    ds.organisationUnits,
+                );
+            });
+        } catch (error) {
+            console.log(
+                `Failed to fetch level ${level} organization units: ${error.message}`,
+            );
+        }
+        return {
+            organisationUnits: uniqBy(organisationUnits, "id"),
+            dataElements: uniq(dataElements),
+        };
+    }
+    async fetchDataSetUnits() {
+        console.log("Fetching data sets units...");
+        const url = `/api/dataSets.json`;
+        const params = {
+            fields: "organisationUnits[id,name]",
             paging: false,
             filter: `id:in:[onFoQ4ko74y,RtEYsASU7PG,ic1BSWhGOso,nGkMm2VBT4G,VDhwrW9DiC1,quMWqLxzcfO,dFRD2A5fdvn,DFMoIONIalm,EBqVAQRmiPm]`,
         };
 
         try {
             const { data } = await this.destApi.get(url, { params });
-            return data.dataSets.flatMap((ds) =>
-                ds.dataSetElements.map((de) => de.dataElement.id),
-            );
+            return data.dataSets.flatMap((ds) => ds.organisationUnits);
         } catch (error) {
             console.log(
-                `Failed to fetch level ${level} organization units: ${error.message}`,
+                `Failed to fetch datasets organization units: ${error.message}`,
             );
         }
     }
@@ -108,7 +137,7 @@ class DHIS2DataTransfer {
     async getOrganisations() {
         try {
             console.log("Fetching organisation units...");
-            const units = await this.fetchOrgUnits(3, "id,name");
+            const units = await Promise.all([this.fetchOrgUnits(5, "id,name")]);
             return units;
         } catch (error) {
             console.error("Failed to fetch organization units:", error.message);
@@ -129,12 +158,13 @@ class DHIS2DataTransfer {
                 { dataValues },
                 {
                     headers: { "Content-Type": "application/json" },
-                    params: { async: true },
+                    params: { async: false },
                 },
             );
-            console.log(data)
+            console.log(JSON.stringify(data.response.importCount));
         } catch (error) {
-           console.log(error.message)
+            console.log(error.response.data.response.importCount);
+            console.log(error.message);
         }
     }
 
@@ -155,7 +185,6 @@ class DHIS2DataTransfer {
             orgUnit: orgUnit.id,
             startDate,
             endDate,
-            children: true,
         });
         datasets.forEach((id) => params.append("dataSet", id));
 
@@ -163,52 +192,46 @@ class DHIS2DataTransfer {
             `Downloading data for ${orgUnit.name} (${current}/${total})...`,
         );
 
-        try {
-            const { data: csvData } = await this.sourceApi.get(
-                `/api/dataValueSets.csv?${params.toString()}`,
-            );
-            console.log(
-                `Finished Downloading data for ${orgUnit.name} (${current}/${total})...`,
-            );
+        const { data: csvData } = await this.sourceApi.get(
+            `/api/dataValueSets.csv?${params.toString()}`,
+        );
+        console.log(
+            `Finished Downloading data for ${orgUnit.name} (${current}/${total})...`,
+        );
 
-            return new Promise((resolve, reject) => {
-                const dataValues = [];
+        return new Promise((resolve, reject) => {
+            const dataValues = [];
 
-                Papa.parse(csvData, {
-                    header: true,
-                    skipEmptyLines: true,
-                    transform: (value) => value.trim(),
-                    step: ({ data }) => {
-                        if (this.isValidDataValue(data)) {
-                            dataValues.push(this.normalizeDataValue(data));
+            Papa.parse(csvData, {
+                header: true,
+                skipEmptyLines: true,
+                transform: (value) => value.trim(),
+                step: ({ data }) => {
+                    if (this.isValidDataValue(data)) {
+                        dataValues.push(this.normalizeDataValue(data));
+                    }
+                },
+                complete: async () => {
+                    try {
+                        const batches = chunk(
+                            dataValues.filter((dv) =>
+                                dataElements.includes(dv.dataElement),
+                            ),
+                            this.batchSize,
+                        );
+                        for (const batch of batches) {
+                            await this.processDataValuesBatch(batch);
                         }
-                    },
-                    complete: async () => {
-                        try {
-                            const batches = chunk(
-                                dataValues.filter((dv) =>
-                                    dataElements.includes(dv.dataElement),
-                                ),
-                                this.batchSize,
-                            );
-
-                            for (const batch of batches) {
-                                await this.processDataValuesBatch(batch);
-                            }
-
-                            resolve({
-                                processed: true,
-                            });
-                        } catch (error) {
-                            reject(error);
-                        }
-                    },
-                    error: reject,
-                });
+                        resolve({
+                            processed: true,
+                        });
+                    } catch (error) {
+                        reject(error);
+                    }
+                },
+                error: reject,
             });
-        } catch (error) {
-            console.log(`Failed to download/process data: ${error.message}`);
-        }
+        });
     }
 
     /**
@@ -254,15 +277,10 @@ class DHIS2DataTransfer {
      */
     async transferData(datasets, startDate, endDate) {
         try {
-            const orgUnits = await this.getOrganisations();
-            const dataElements = await this.fetchDataElements();
-            let totalImported = 0;
-            let totalUpdated = 0;
-            let totalIgnored = 0;
-            let totalDeleted = 0;
+            const { dataElements, organisationUnits } =
+                await this.fetchDataElements(datasets);
             let errors = [];
-
-            for (const [index, orgUnit] of orgUnits.entries()) {
+            for (const [index, orgUnit] of organisationUnits.entries()) {
                 try {
                     await this.downloadCSV(
                         datasets,
@@ -270,7 +288,7 @@ class DHIS2DataTransfer {
                         startDate,
                         endDate,
                         index + 1,
-                        orgUnits.length,
+                        organisationUnits.length,
                         dataElements,
                     );
                 } catch (error) {
@@ -284,15 +302,7 @@ class DHIS2DataTransfer {
                     );
                 }
             }
-
-            return {
-                totalImported,
-                totalUpdated,
-                totalIgnored,
-                totalDeleted,
-                totalOrgUnits: orgUnits.length,
-                errors: errors.length ? errors : undefined,
-            };
+            return errors;
         } catch (error) {
             console.log(`Transfer failed: ${error.message}`);
         }
@@ -300,42 +310,38 @@ class DHIS2DataTransfer {
 }
 
 async function main() {
-    try {
-        const configs = {
-            source: {
-                url: process.env.SOURCE_DHIS2_URL,
-                username: process.env.SOURCE_DHIS2_USERNAME,
-                password: process.env.SOURCE_DHIS2_PASSWORD,
-            },
-            dest: {
-                url: process.env.DEST_DHIS2_URL,
-                username: process.env.DEST_DHIS2_USERNAME,
-                password: process.env.DEST_DHIS2_PASSWORD,
-            },
-        };
+    const configs = {
+        source: {
+            url: process.env.SOURCE_DHIS2_URL,
+            username: process.env.SOURCE_DHIS2_USERNAME,
+            password: process.env.SOURCE_DHIS2_PASSWORD,
+        },
+        dest: {
+            url: process.env.DEST_DHIS2_URL,
+            username: process.env.DEST_DHIS2_USERNAME,
+            password: process.env.DEST_DHIS2_PASSWORD,
+        },
+    };
 
-        const datasets = [
-            "onFoQ4ko74y",
-            "RtEYsASU7PG",
-            "ic1BSWhGOso",
-            "nGkMm2VBT4G",
-            "VDhwrW9DiC1",
-            "quMWqLxzcfO",
-            "dFRD2A5fdvn",
-            "DFMoIONIalm",
-            "EBqVAQRmiPm",
-        ];
+    const datasets = [
+        "onFoQ4ko74y",
+        "RtEYsASU7PG",
+        "ic1BSWhGOso",
+        "nGkMm2VBT4G",
+        "VDhwrW9DiC1",
+        "quMWqLxzcfO",
+        "dFRD2A5fdvn",
+        "DFMoIONIalm",
+        "EBqVAQRmiPm",
+    ];
 
-        const transfer = new DHIS2DataTransfer(configs.source, configs.dest);
-        const result = await transfer.transferData(
-            datasets,
-            "2024-10-01",
-            "2024-12-31",
-        );
-        console.log("Transfer completed:", result);
-    } catch (error) {
-        console.error("Transfer failed:", error.message);
-    }
+    const transfer = new DHIS2DataTransfer(configs.source, configs.dest);
+    const result = await transfer.transferData(
+        datasets,
+        "2024-10-01",
+        "2024-12-31",
+    );
+    console.log("Transfer completed:", result);
 }
 
 if (require.main === module) {
